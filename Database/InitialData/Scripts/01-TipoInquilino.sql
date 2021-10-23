f0b80e43959f06228269a51ceb7596889e629547
fsoza1976 <fsoza1976@gmail.com>
2021-10-22 19:03:19 -0600

    Creación scripts.

---
+++ Database/InitialData/Scripts/01-TipoInquilino.sql
@@ -0,0 +1,41 @@
+﻿DROP TABLE IF EXISTS #TipoTemp
+
+SELECT
+Id_TipoInquilino, Descripcion, Estado INTO #TipoTemp
+FROM (
+VALUES 
+(1, 'FISICO', true),
+(2, 'JURIDICO', true)
+)AS TEMP(Id_TipoInquilino, Descripcion, Estado)
+
+
+
+
+
+
+--ACTUALIZAR DATOS:
+UPDATE TI SET 
+TI.Descripcion=TT.Descripcion,
+TI.Estado=TT.Estado
+FROM dbo.TipoInquilino TI
+INNER JOIN #TipoTemp TT
+ON TI.Id_TipoInquilino=TT.Id_TipoInquilino
+
+
+
+--INSERTAR DATOS:
+SET IDENTITY_INSERT dbo.TipoInquilino ON
+
+INSERT INTO dbo.TipoInquilino(
+Id_TipoInquilino, Descripcion, Estado)
+SELECT 
+Id_TipoInquilino, Descripcion, Estado
+FROM #TipoTemp
+
+EXCEPT
+SELECT
+Id_TipoInquilino, Descripcion, Estado
+FROM dbo.TipoInquilino
+
+SET IDENTITY_INSERT dbo.TipoInquilino OFF
+GO
